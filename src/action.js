789d97d53d70e627730b141f9d8f24cd5a8b743d
dogura <daiki.ogura@soliton.co.jp>
2016-01-21 19:37:45 +0900

    add utility function to execute authorized request

--- src/action.js
+++ src/action.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+import {getAuthToken, isTokenValid} from './util/auth';
+
 
 export function EventHandler(func) {
   return (ctx, component) => {
@@ -5,4 +9,19 @@ export function EventHandler(func) {
       return func.call('do not use "this"', ctx, component.props, component.state, ...args);
     };
   };
+}
+
+
+/**
+ * @param {string} [baseURL=/api/] api baseURL
+ * @param {string} [tokenField=X-Account-Token] http header field of authorization token
+ * @returns {Tuple.<axios,Error>} axios and error
+ */
+export function authorizedRequest(baseURL = '/api/', tokenField = 'X-Account-Token') {
+  const token = getAuthToken();
+  const [isValid, err] = isTokenValid(token);
+  if (!isValid) {
+    return [null, new Error(err)];
+  }
+  return [axios.create({baseURL: baseURL, headers: {[tokenField]: token}}), null];
 }
