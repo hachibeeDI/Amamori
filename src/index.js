15b34f431fac19074e9eec9db9b072af56e893e4
dogura <daiki.ogura@soliton.co.jp>
2016-02-10 15:08:40 +0900

    fix import style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {EventHandler} from './action';
 import CreateDispatcher from './dispatcher';
 import Store from './store';
-import View from './view';
+import {View} from './view';
 
 export {EventHandler, CreateDispatcher, Store, View};
