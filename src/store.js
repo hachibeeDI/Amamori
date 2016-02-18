26dc97e8e901d95850d22b46ff0a1cb5952211a2
dogura <daiki.ogura@soliton.co.jp>
2016-02-18 15:25:05 +0900

    remove unused example

--- src/store.js
+++ src/store.js
@@ -3,33 +3,6 @@ import {EventEmitter} from 'events';
 import {clearAuthToken} from './util/auth'
 
 
-/**
- * Example:
- *
- * class SystemToolStore extends Store {
- *   data = '';
- * }
- *
- * class HogeComponent extends Viwe {
- *   constructor(props) {
- *     super(props);
- *     const self = this;
- *     const store = new HogeStore(HogeDispatcher);
- *     this
- *       .observe(store)
- *       .on(subscribe => {
- *         subscribe('toolChanged', (targVal) => {
- *           if (targVal === this.data) {
- *             self.setState({result: targVal});
- *           }
- *         });
- *     });
- *     this.state = {
- *       result: 'result viewer'
- *     };
- *   }
- * }
- */
 export default class Store extends EventEmitter {
   /**
    * @param {flux.Dispactcher} dispatcher dispatcher
