8b27fe386afbcb588ba262fcc3b5b352b08723dd
dogura <daiki.ogura@soliton.co.jp>
2016-02-18 15:16:27 +0900

    add methods to define observers
    This change make subclass should no longer override constructor.

--- src/store.js
+++ src/store.js
@@ -38,19 +38,30 @@ export default class Store extends EventEmitter {
   constructor(dispatcher) {
     super();
     this.dispatcher = dispatcher;
-    this.dispatcher.on('error', (response) => {
+    const d = this.dispatcher;
+    d.on('error', (response) => {
       console.error(response);
       this.emit('onError', response);
     });
-    this.dispatcher.on('loginSuspend', (e) => {
+    d.on('loginSuspend', (e) => {
       clearAuthToken();
       window.sessionStorage.setItem('lastLocation', window.location.pathname);
       this.emit('loginSuspend');
     });
+    this.observeres(d.on.bind(d))
   }
 
   publishError(message) {
     this.emit('onError', {data: {message}});
+  }
+
+  /**
+   * this method is called once in the constructor.
+   * should override.
+   * @param {function(name:string, listener:function(...any))} subscribe will observe the dispatcher events.
+   * @returns {void}
+   */
+  observeres(subscribe) {
   }
 
   /**
